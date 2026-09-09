8e302c717b0341259afa32150ab478c62c30f627
abedoulaye <abdullahdia123@gmail.com>
2026-09-09 11:56:35 -0400

    fix: typescript interfaces match go structs

--- backend/models.go
+++ backend/models.go
@@ -15,7 +15,7 @@ type Product struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
 	Description string `json:"description"`
-	PriceInCents int64 `json:"price"`
+	PriceInCents int64 `json:"price_cents"`
 	Stock int `json:"stock"`
 	Image string `json:"image"`
 }
