f5f04ee8e5200a2f601f1ded2f786c62f228d4a9
abedoulaye <abdullahdia123@gmail.com>
2026-09-12 15:57:45 -0400

    refactor: renamed app's name to diariata's fabrics

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-brand">
-        Perle d'Afrique
+        Diariata's Fabrics
       </Link>
 
       <div className="navbar-links">
