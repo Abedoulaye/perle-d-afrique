7dc502095dc79932e76ef1f1ca1aedbeaf4b5771
abedoulaye <abdullahdia123@gmail.com>
2026-09-21 10:54:51 -0400

    feat: set default empty strings for shipping columns

---
+++ backend/migrations/006_shipping_not_null.sql
@@ -0,0 +1,3 @@
+ALTER TABLE orders ALTER COLUMN shipping_name SET DEFAULT '';
+ALTER TABLE orders ALTER COLUMN shipping_address SET DEFAULT '';
+ALTER TABLE orders ALTER COLUMN shipping_phone SET DEFAULT '';
